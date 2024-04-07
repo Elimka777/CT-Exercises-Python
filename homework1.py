88c4288dbe99824702da71df263c81a72ee2c672
Elima777 <elimazholdubaeva@gmail.com>
2024-04-07 07:08:09 -0500

    Add My First Homework

---
+++ homework1.py
@@ -0,0 +1,17 @@
+'''
+Exercise 1: Dance to the Python Rhythm!
+
+1. Create a multi-line string using triple quotes. This string should be a small paragraph about your favourite hobby, passion, or general thought.
+2. Write a Python statement to perform a simple calculation,such as adding two numbers. Add a comment explaining the purpose of this statement.
+'''
+
+favourite_hobby = '''
+Drawing can settle me down when I'm anxious; 
+it has the ability to whisk me away me from this world to a completely different; 
+create a path to my inner peace.
+'''
+
+print(favourite_hobby)
+
+sum = 10 + 9
+print("The sum of 10 and 9 is:", sum)
