ba83637c5b0079ba084cc2a0ebad57ca3a9fa18b
Elima777 <elimazholdubaeva@gmail.com>
2024-04-16 16:11:30 -0500

    added new assignment

---
+++ exercisesCT2.py
@@ -0,0 +1,12 @@
+flour_per_cake = 250
+total_flour = 2.5 * 1000
+numbers_of_cakes = total_flour / flour_per_cake
+print(numbers_of_cakes)
+
+kingdom = "Pythonland"
+print(kingdom)
+
+shirt1 = 45
+shirt2 = 50
+result = shirt1 < shirt2
+print(result)
